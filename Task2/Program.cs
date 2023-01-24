c4d8c6534bbb14c5019a9d8282a3854ecc4f762e
Daria <maivery@mail.ru>
2023-01-25 01:02:13 +0800

    add Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.*/
+
+Console.WriteLine("Введите число:");
+int GetNumber()
+{
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int a = GetNumber ();
+if (a / 100 == 0)
+{
+    Console.WriteLine("Третьей цифры нет"); 
+}
+else
+{
+    while (a > 999)
+    {
+        a = a /10;
+    }
+    a = a % 10;
+    Console.WriteLine( a );
+}
+
